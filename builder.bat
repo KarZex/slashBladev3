93886af9138df5760314faef3895aa7624b63a98
Kar Zex <152583031+KarZex@users.noreply.github.com>
2025-08-13 17:11:33 +0900

    tuyi

--- builder.bat
+++ builder.bat
@@ -12,8 +12,8 @@ cd ..
 python builder.py
 "C:\Program Files\7-Zip\7z.exe" a -tzip ./output/SlashBladeV4B.zip .\behavior_packs\SlashBladeV4
 "C:\Program Files\7-Zip\7z.exe" a -tzip ./output/SlashBladeV4R.zip .\resource_packs\SlashBladeV4
-rename output\SlashBladeV4B.zip SlashBladeV4B.mcpack
-rename output\SlashBladeV4R.zip SlashBladeV4R.mcpack
+rename output\SlashBladeV4B.zip SakuraSlashBladeV4B.mcpack
+rename output\SlashBladeV4R.zip SakuraSlashBladeV4R.mcpack
 rd /S /Q behavior_packs\SlashBladeV4
 rd /S /Q resource_packs\SlashBladeV4
 del output\SlashBladeV4B.zip
