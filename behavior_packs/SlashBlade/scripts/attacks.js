be822c0a832b442c976d447eddb1d5493c840226
しなもん <147789317+Densetsu4545@users.noreply.github.com>
2026-07-28 14:14:14 +0900

    yug

--- behavior_packs/SlashBlade/scripts/attacks.js
+++ behavior_packs/SlashBlade/scripts/attacks.js
@@ -54,7 +54,7 @@ export function SummonedswordLine( user,blade,bladeSlot ){
         y: V.y * power,
         z: V.z * power 
         }
-        const color = bladeData[`${blade.typeId.split(`:`)[1]}`]["color"]
+        const color = bladeData[`${blade.typeId.split(`:`)[1]}`]["color"];
         const fire = user.dimension.spawnEntity(`safire:summonedsword`,FirePos);
         fire.getComponent(`minecraft:projectile`).owner = user
         fire.triggerEvent(`${color}`);
